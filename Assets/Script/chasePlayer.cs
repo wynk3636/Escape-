5e6471fb3ace8fdf266192a9b2ccd1196e247ef6
ishida <h_hand_3636@gmail.com>
2020-10-25 13:57:15 +0900

    chase enemy

---
+++ Assets/Script/chasePlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chasePlayer : MonoBehaviour
+{
+
+    public string targetObjectName;
+    public float speed = 1;
+
+    GameObject targetObject;
+    Rigidbody2D rbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetObject = GameObject.Find(targetObjectName);
+
+        //回転抑止
+        rbody = GetComponent<Rigidbody2D>();
+        rbody.gravityScale = 0;
+        rbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 targetDirect = (targetObject.transform.position - this.transform.position).normalized;
+
+        //進む量
+        float vx = targetDirect.x * speed;
+        float vy = targetDirect.y * speed;
+        rbody.velocity = new Vector2(vx, vy);
+
+        //移動方向で左右に向きを変える
+        this.GetComponent<SpriteRenderer>().flipX = (vx > 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            gameStateManager.gameState = false;
+        }
+    }
+}
